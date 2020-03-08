0fb46490fa2bf6cb659839eaec44747ccb9001ae
BJSY <bianjingshiyi@163.com>
2020-03-09 01:54:43 +0800

    新增ITriggerManager。

--- Interfaces/ITriggerManager.cs
+++ Interfaces/ITriggerManager.cs
@@ -5,10 +5,17 @@ namespace TouhouCardEngine.Interfaces
 {
     public interface ITriggerManager
     {
+        void register(string eventName, ITrigger trigger);
+        ITrigger[] getTriggers(string eventName);
+        Task doEvent(string eventName, object[] args);
         Task doEvent<T>(T eventArg, Func<T, Task> action) where T : IEventArg;
+    }
+    public interface ITrigger
+    {
+        Task invoke(object[] args);
     }
     public interface IEventArg
     {
-
+        string name { get; }
     }
 }
