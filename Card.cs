600a9492071dbc557e334ace84e2fc3be3b499ac
BJSY <bianjingshiyi@hotmail.com>
2020-07-13 18:56:04 +0800

    GeneratedBuff.cs单独文件。

--- Card.cs
+++ Card.cs
@@ -4,25 +4,6 @@ using System.Collections.Generic;
 using TouhouCardEngine.Interfaces;
 namespace TouhouCardEngine
 {
-    public class GeneratedBuff : Buff
-    {
-        public override int id { get; } = 0;
-        public override PropModifier[] modifiers { get; }
-        public GeneratedBuff(int id, params PropModifier[] modifiers)
-        {
-            this.id = id;
-            this.modifiers = modifiers;
-        }
-        GeneratedBuff(GeneratedBuff origin)
-        {
-            id = origin.id;
-            modifiers = origin.modifiers;
-        }
-        public override Buff clone()
-        {
-            return new GeneratedBuff(this);
-        }
-    }
     [Serializable]
     public class Card : ICard
     {
---
+++ GeneratedBuff.cs
@@ -0,0 +1,22 @@
+﻿namespace TouhouCardEngine
+{
+    public class GeneratedBuff : Buff
+    {
+        public override int id { get; } = 0;
+        public override PropModifier[] modifiers { get; }
+        public GeneratedBuff(int id, params PropModifier[] modifiers)
+        {
+            this.id = id;
+            this.modifiers = modifiers;
+        }
+        GeneratedBuff(GeneratedBuff origin)
+        {
+            id = origin.id;
+            modifiers = origin.modifiers;
+        }
+        public override Buff clone()
+        {
+            return new GeneratedBuff(this);
+        }
+    }
+}
