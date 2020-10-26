5756bfed1d7c170349b397aaad38fafce1781e15
bianjingshiyi <bianjingshiyi@hotmail.com>
2020-10-26 14:08:17 +0800

    网络测试更改。

--- Room/LocalRoom.cs
+++ Room/LocalRoom.cs
@@ -13,15 +13,23 @@ namespace TouhouCardEngine
             this.logger = logger;
             networking = new ClientNetworking(logger);
         }
-        public async Task createRoom(RoomPlayerData playerData)
+        public void createLocalRoom()
+        {
+            logger?.log("客户端创建本地房间");
+            room = new LocalRoom();
+            localPlayer = room.addLocalPlayer().Result;
+        }
+        public async Task createOnlineRoom(RoomPlayerData playerData)
         {
             logger?.log("客户端创建房间");
-            await networking.invoke<RoomData>(nameof(createRoom), playerData);
+            await networking.invoke<RoomData>("createRoom", playerData);
         }
         public void Dispose()
         {
-
+            if (networking != null)
+                networking.Dispose();
         }
+        public RoomPlayer localPlayer { get; private set; } = null;
         public ClientRoom room { get; private set; } = null;
         public ClientNetworking networking { get; }
         #endregion
@@ -29,27 +37,31 @@ namespace TouhouCardEngine
         ILogger logger { get; }
         #endregion
     }
-    public class ClientRoom : TypedRoom
+    public class ClientRoom : Room
+    {
+        /// <summary>
+        /// 添加并返回本地玩家。
+        /// </summary>
+        /// <returns></returns>
+        public Task<RoomPlayer> addLocalPlayer()
+        {
+            RoomPlayer player = new LocalRoomPlayer(++lastPlayerId);
+            addPlayer(new RoomPlayerData("本地玩家", RoomPlayerType.human), player);
+            return Task.FromResult(player);
+        }
+    }
+    public class OnlineRoom : Room
     {
         ClientNetworking _networking;
-        public ClientRoom(ClientNetworking networking, RoomData data) : base(data)
+        public OnlineRoom(ClientNetworking networking, RoomData data) : base(data)
         {
             _networking = networking;
         }
-        public override Task<RoomPlayerData> addAIPlayer()
-        {
-            return _networking.invoke<RoomPlayerData>(nameof(Room.addAIPlayer));
-        }
     }
-    public class LocalRoom : TypedRoom
+    public class LocalRoom : ClientRoom
     {
         public LocalRoom() : base()
         {
-        }
-        public override RoomData data => throw new NotImplementedException();
-        protected override void setData(RoomData data)
-        {
-            throw new NotImplementedException();
         }
     }
 }
