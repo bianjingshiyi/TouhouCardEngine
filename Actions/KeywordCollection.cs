29ce5169117cba08af19e563ed452c8d22964867
bjsy <bianjingshiyi@hotmail.com>
2022-08-24 14:33:35 +0800

    新增KeywordCollection

---
+++ Actions/KeywordCollection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+namespace TouhouCardEngine
+{
+    [Serializable]
+    public class KeywordCollection : List<string>
+    {
+        public KeywordCollection() : this(null)
+        {
+        }
+        public KeywordCollection(IEnumerable<string> keywords)
+        {
+            if (keywords != null)
+                AddRange(keywords);
+        }
+    }
+}
