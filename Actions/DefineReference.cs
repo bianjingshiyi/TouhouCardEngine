22c019504e0fe658a3c3f48b55ffc69e7a98d1e3
bjsy <bianjingshiyi@hotmail.com>
2022-12-04 01:03:38 +0800

    重写了DefineReference的Equels方法，新增一些构造方法重载

--- Actions/DefineReference.cs
+++ Actions/DefineReference.cs
@@ -9,11 +9,41 @@ namespace TouhouCardEngine
             this.cardPoolId = cardPoolId;
             this.defineId = defineId;
         }
+        public DefineReference(CardDefine cardDefine) :
+            this(cardDefine.cardPoolId, cardDefine.id)
+        {
+        }
+        public DefineReference(BuffDefine buffDefine) :
+            this(buffDefine.cardPoolId, buffDefine.id)
+        {
+        }
         public DefineReference() : this(0, 0)
         {
         }
+        public override bool Equals(object obj)
+        {
+            if (obj is DefineReference other)
+            {
+                return cardPoolId == other.cardPoolId && defineId == other.defineId;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            //不知道从哪里抄的异或哈希算法，使用了质数。
+            int hashCode = 17;
+            hashCode = hashCode * 31 + cardPoolId.GetHashCode();
+            hashCode = hashCode * 31 + defineId;
+            return hashCode;
+        }
+        public override string ToString()
+        {
+            return string.Format("({0}, {1})", cardPoolId, defineId);
+        }
         public long cardPoolId;
         public int defineId;
+
+        public static DefineReference Empty = new DefineReference(0, 0);
     }
     [Obsolete("use DefineReference instead")]
     [Serializable]
