b872f6e2e790d8bde8329cf2365064e9eede2271
Jim Jiang <jim@lotlab.org>
2022-12-11 10:38:35 +0800

    fix: 修正 DefineReference "==" 运算符不重载的问题

--- Actions/DefineReference.cs
+++ Actions/DefineReference.cs
@@ -33,13 +33,22 @@ namespace TouhouCardEngine
             //不知道从哪里抄的异或哈希算法，使用了质数。
             int hashCode = 17;
             hashCode = hashCode * 31 + cardPoolId.GetHashCode();
-            hashCode = hashCode * 31 + defineId;
+            hashCode = hashCode * 31 + defineId.GetHashCode();
             return hashCode;
         }
         public override string ToString()
         {
             return string.Format("({0}, {1})", cardPoolId, defineId);
         }
+        public static bool operator==(DefineReference r1, DefineReference r2)
+        {
+            return r1.Equals(r2);
+        }
+        public static bool operator!=(DefineReference r1, DefineReference r2)
+        {
+            return !r1.Equals(r2);
+        }
+
         public long cardPoolId;
         public int defineId;
 
