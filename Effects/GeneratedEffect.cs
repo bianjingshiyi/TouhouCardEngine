f2002672ea03375a20559b7f64395a4b2a36a803
Cuerzor <773785114@qq.com>
2023-04-21 18:44:40 +0800

    将GeneratedEffect变为抽象类。

--- Effects/GeneratedEffect.cs
+++ Effects/GeneratedEffect.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using TouhouCardEngine.Interfaces;
 namespace TouhouCardEngine
 {
-    public class GeneratedEffect : IPassiveEffect
+    public abstract class GeneratedEffect : IEffect
     {
         #region 公有方法
         #region 构造方法
@@ -24,47 +24,13 @@ namespace TouhouCardEngine
                 var flow = new Flow(game, card, buff, null);
                 await game.runActions(flow, onEnableAction);
             }
-            foreach (var entryNode in triggerList)
-            {
-                string triggerName = getEffectName(card, buff, entryNode.eventName);
-                game.logger.log("Effect", card + "注册触发器" + triggerName);
-                Trigger trigger = new Trigger(
-                    args =>
-                    {
-                        var condition = entryNode.getTriggerCondtionValuePort();
-                        if (condition == null || condition.getConnectedOutputPort() == null)
-                            return true;
 
-                        var flow = new Flow(game, card, buff, args.OfType<EventArg>().FirstOrDefault());
-                        Task<bool> task = game.getValue<bool>(flow, condition);
-
-                        if (task.IsCompleted)
-                            return task.Result;
-                        else
-                            throw new InvalidOperationException("无法在触发器条件中执行需要等待的动作");
-                    },
-                    args =>
-                    {
-                        var flow = new Flow(game, card, buff, args.OfType<IEventArg>().FirstOrDefault());
-                        return game.runActions(flow, entryNode.getActionOutputPort());
-                    }, name: triggerName);
-                await card.setProp(game, triggerName, trigger);
-                game.triggers.registerDelayed(entryNode.eventName, trigger);
-            }
             // 设置该Effect已被启用。
             string disableName = getEffectName(card, buff, "Disabled");
             await card.setProp(game, disableName, false);
         }
         public virtual async Task onDisable(IGame game, ICard card, IBuff buff)
         {
-            foreach (var graph in triggerList)
-            {
-                string triggerName = getEffectName(card, buff, graph.eventName);
-                game.logger.log("Effect", card + "注销触发器" + triggerName);
-                Trigger trigger = card.getProp<Trigger>(game, triggerName);
-                await card.setProp(game, triggerName, null);
-                game.triggers.remove(graph.eventName, trigger);
-            }
             // 设置该Effect已被禁用。
             string disableName = getEffectName(card, buff, "Disabled");
             await card.setProp(game, disableName, true);
@@ -79,6 +45,10 @@ namespace TouhouCardEngine
         {
             string disableName = getEffectName(card, buff, "Disabled");
             return card.getProp<bool>(game, disableName);
+        }
+        public ActionNode getDataNode()
+        {
+            return graph?.findActionNode(GeneratedEffectData.defName);
         }
         public T getProp<T>(string name)
         {
@@ -124,84 +94,11 @@ namespace TouhouCardEngine
         }
         public virtual void Init()
         {
-            graph.createActionNode(GeneratedEffectData.defName, 0, 500);
-            graph.createTriggerEntryNode("ActiveEvent", 0, 0);
+            graph.createActionNode(GeneratedEffectData.defName);
         }
-        public virtual SerializableEffect Serialize()
-        {
-            throw new NotImplementedException();
-        }
-        public ActionNode getDataNode()
-        {
-            return graph?.findActionNode(GeneratedEffectData.defName);
-        }
-        public virtual bool checkCondition(IGame game, ICard card, IBuff buff, IEventArg eventArg)
-        {
-            var trigger = triggerList.FirstOrDefault(t => t.eventName == game.triggers.getName(eventArg));
-            if (trigger != null)
-            {
-                var condition = trigger.getTriggerCondtionValuePort();
-                if (condition == null || condition.getConnectedOutputPort() == null)
-                    return true;
-
-                var flow = new Flow(game, card, buff, eventArg);
-                var task = game.getValue<bool>(flow, condition);
-                if (task.IsCompleted)
-                {
-                    return task.Result;
-                }
-                else
-                    throw new InvalidOperationException("不能在条件中调用需要等待的动作");
-            }
-            else
-                return false;
-        }
-        /// <summary>
-        /// 检查目标卡牌是否是效果的合法目标
-        /// </summary>
-        /// <param name="game"></param>
-        /// <param name="card">目标卡牌</param>
-        /// <param name="buff"></param>
-        /// <param name="eventArg"></param>
-        /// <param name="invalidMsg"></param>
-        /// <returns></returns>
-        public virtual bool checkTarget(IGame game, ICard card, IBuff buff, IEventArg eventArg, out string invalidMsg)
-        {
-            var trigger = triggerList.FirstOrDefault(t => t.eventName == game.triggers.getName(eventArg));
-            if (trigger != null)
-            {
-                invalidMsg = null;
-                //如果触发不包含任何目标，那么目标肯定不是合法目标
-                if (trigger.targetCheckerList == null || trigger.targetCheckerList.Count < 1)
-                    return false;
-                foreach (var targetChecker in trigger.targetCheckerList)
-                {
-                    //检查目标条件
-                    if (!targetChecker.isValidTarget(game, card, buff, eventArg, out invalidMsg))
-                    {
-                        //有条件没有通过，不是合法目标
-                        return false;
-                    }
-                }
-                //有目标并且没有条件不通过或者没有条件，返回真
-                return true;
-            }
-            else
-            {
-                invalidMsg = null;
-                return false;
-            }
-        }
-        public virtual Task execute(IGame game, ICard card, IBuff buff, IEventArg eventArg)
-        {
-            var trigger = triggerList.FirstOrDefault(t => t.eventName == game.triggers.getName(eventArg));
-            if (trigger != null)
-            {
-                var flow = new Flow(game, card, buff, eventArg);
-                return game.runActions(flow, trigger.getActionOutputPort());
-            }
-            return Task.CompletedTask;
-        }
+        public abstract bool checkCondition(IGame game, ICard card, IBuff buff, IEventArg eventArg);
+        public abstract Task execute(IGame game, ICard card, IBuff buff, IEventArg eventArg);
+        public abstract SerializableEffect Serialize();
         #endregion
         #region 私有方法
         protected virtual IEnumerable<ITraversable> getTraversableProps()
@@ -210,10 +107,6 @@ namespace TouhouCardEngine
                 yield return onEnableAction;
             if (onDisableAction != null)
                 yield return onDisableAction;
-            foreach (var trigger in triggerList)
-            {
-                yield return trigger;
-            }
         }
         protected string getEffectName(ICard card, IBuff buff, string eventName)
         {
@@ -225,12 +118,8 @@ namespace TouhouCardEngine
         public string name;
         public ActionGraph graph { get; set; }
         public Dictionary<string, object> propDict = new Dictionary<string, object>();
-        protected virtual PileNameCollection pileList => getDataNode()?.getConst<PileNameCollection>(GeneratedEffectData.pileListName);
-        public virtual ControlOutput onEnableAction => getDataNode()?.getOutputPort<ControlOutput>(GeneratedEffectData.enableActionName);
-        public virtual ControlOutput onDisableAction => getDataNode()?.getOutputPort<ControlOutput>(GeneratedEffectData.disableActionName);
-        public virtual IEnumerable<TriggerEntryNode> triggerList => graph?.nodes?.OfType<TriggerEntryNode>();
-
-        public string[] piles => pileList?.ToArray() ?? Array.Empty<string>();
+        public ControlOutput onEnableAction => getDataNode()?.getOutputPort<ControlOutput>(GeneratedEffectData.enableActionName);
+        public ControlOutput onDisableAction => getDataNode()?.getOutputPort<ControlOutput>(GeneratedEffectData.disableActionName);
         #endregion
     }
 
