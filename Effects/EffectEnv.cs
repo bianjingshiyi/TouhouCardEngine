9e11edc9449f46058d448a108eaa560607579f7c
Cuerzor58 <773785114@qq.com>
2024-06-20 01:42:37 +0800

    为EffectEnv类添加arguments字段。

--- Effects/EffectEnv.cs
+++ Effects/EffectEnv.cs
@@ -1,4 +1,6 @@
-﻿using TouhouCardEngine.Interfaces;
+﻿using System.Collections.Generic;
+using System.Linq;
+using TouhouCardEngine.Interfaces;
 
 namespace TouhouCardEngine
 {
@@ -12,14 +14,30 @@ namespace TouhouCardEngine
             this.eventArg = eventArg;
             this.effect = effect;
         }
+        public void setArgument(string name, object value)
+        {
+            arguments[name] = value;
+        }
+        public object getArgument(string name)
+        {
+            if (arguments.TryGetValue(name, out var value))
+                return value;
+            return null;
+        }
         public FlowEnv toFlowEnv()
         {
-            return new FlowEnv(game, card, buff, eventArg, effect);
+            var env = new FlowEnv(game, card, buff, eventArg, effect);
+            foreach (var pair in arguments)
+            {
+                env.SetArgument(pair.Key, pair.Value);
+            }
+            return env;
         }
         public CardEngine game;
         public Card card;
         public Buff buff;
         public EventArg eventArg;
         public Effect effect;
+        private Dictionary<string, object> arguments = new Dictionary<string, object>();
     }
 }
